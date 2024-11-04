46299b4c91acbe3a1632edaf7bc2d88ecb67614d
Harsha <yhvreddyinfo@gmail.com>
2024-11-04 07:13:47 +0530

    done this module

--- util/database.js
+++ util/database.js
@@ -1,9 +1,8 @@
-const mysql = require("mysql2");
-const pool = mysql.createPool({
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("node-complete", "root", "Mylife@17101994", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  password: "Mylife@17101994",
-  database: "node-complete",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;

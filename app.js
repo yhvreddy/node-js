8d93cdeae8cbea3bc78c7332ccb5a120f63f07fb
Harsha <yhvreddyinfo@gmail.com>
2024-11-10 19:51:01 +0530

    added files

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const MongoDBStore = require("connect-mongodb-session")(session);
 const errorController = require("./controllers/error");
 const User = require("./models/user");
 
-const MONGODB_URI = "";
+const MONGODB_URI = "mongodb://localhost:27017/node-complete";
 
 const app = express();
 const store = new MongoDBStore({
@@ -47,8 +47,8 @@ mongoose
     User.findOne().then((user) => {
       if (!user) {
         const user = new User({
-          name: "Max",
-          email: "max@test.com",
+          name: "Harsha",
+          email: "harsha@test.com",
           cart: {
             items: [],
           },
